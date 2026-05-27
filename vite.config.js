a54f111d3d3273c737a323a0701746e8a86c25bb
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2026-05-27 11:13:56 +0200

    Fix distribution

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// When deployed via GitHub Actions, GITHUB_REPOSITORY is "user/repo-name"
-const base = process.env.GITHUB_REPOSITORY
-  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-  : '/'
-
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: './',
 })
